4b8102de667b041d3e6f5721e79bc908434bb8f2
Edwidge Amisial <eamisial@deloitte.com>
2023-06-22 14:17:43 -0400

    End of Section 2

---
+++ Reactivities.Persistence/ReactivitiesContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain;
+
+namespace Reactivities.Persistence;
+
+public class ReactivitiesContext : DbContext
+{
+  public ReactivitiesContext(DbContextOptions<ReactivitiesContext> options) : base(options)  { }
+
+  public DbSet<Activity> Activities { get; set; }
+}
